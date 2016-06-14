dc6100d0e420281fb0168a7b3d020d978e42b25d
Dmitriy Borysovets <dzmitry.borysovets@gmail.com>
2016-06-14 21:35:20 +0300

    add validation

--- src/main/java/by/eftech/webapp/web/OrderController.java
+++ src/main/java/by/eftech/webapp/web/OrderController.java
@@ -6,7 +6,10 @@ import by.eftech.webapp.service.OrderService;
 import by.eftech.webapp.utils.EmailSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.support.SessionStatus;
@@ -25,33 +28,39 @@ public class OrderController {
     private OrderService service;
 
     @RequestMapping(value = "/checkout", method = RequestMethod.GET)
-    public String getCheckout(HttpSession session) {
+    public String getCheckout(Model model) {
+        model.addAttribute("order", new SellerOrder());
         return "checkout";
     }
 
 
     @RequestMapping(value = "/continue-order", method = RequestMethod.POST)
-    public String continueOrder(@Valid SellerOrder order, HttpSession session, BindingResult result, SessionStatus status) throws IOException {
-        status.setComplete();
-        List<Item> items = (List<Item>) session.getAttribute("cart");
-        order.setItems(items);
-        service.save(order);
-        EmailSender sender = EmailSender.getInstance();
-        StringBuilder builder = new StringBuilder();
-        builder.append("From: ").append(order.getFullname()).append(" ").append(order.getLastname());
-        builder.append("\n");
-        builder.append("Company: ").append(order.getCompanyName());
-        builder.append("\n");
-        builder.append("Country: ").append(order.getCountry());
-        builder.append("\n");
-        builder.append("Orders: ");
-        builder.append("\n");
-        for (int i = 0; i < order.getItems().size(); i++) {
-            builder.append(i + 1).append(". ").append(order.getItems().get(i).getTruckMining().getModel().getName()).append("\n");
-            items.remove(i);
-            i--;
+    public String continueOrder(@ModelAttribute("order") @Valid SellerOrder order, BindingResult result, HttpSession session, SessionStatus status, ModelMap model) throws IOException {
+        if (result.hasErrors()) {
+            return "checkout";
+        }else {
+            status.setComplete();
+            List<Item> items = (List<Item>) session.getAttribute("cart");
+            order.setItems(items);
+            service.save(order);
+            EmailSender sender = EmailSender.getInstance();
+            StringBuilder builder = new StringBuilder();
+            builder.append("From: ").append(order.getFullname()).append(" ").append(order.getLastname());
+            builder.append("\n");
+            builder.append("Company: ").append(order.getCompanyName());
+            builder.append("\n");
+            builder.append("Country: ").append(order.getCountry());
+            builder.append("\n");
+            builder.append("Orders: ");
+            builder.append("\n");
+            for (int i = 0; i < order.getItems().size(); i++) {
+                builder.append(i + 1).append(". ").append(order.getItems().get(i).getTruckMining().getModel().getName()).append("\n");
+                items.remove(i);
+                i--;
+            }
+            sender.sendEmail(order.getEmailAddress(), builder.toString());
         }
-        sender.sendEmail(order.getEmailAddress(), builder.toString());
+
         return "redirect:/category";
     }
 
