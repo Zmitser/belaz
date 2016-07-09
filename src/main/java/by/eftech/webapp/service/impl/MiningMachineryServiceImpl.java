aec35a0d9499e53bc0c0db15c13a2389604d75b2
Dmitriy Borysovets <dzmitry.borysovets@gmail.com>
2016-07-09 13:08:40 +0300

    add titles

---
+++ src/main/java/by/eftech/webapp/service/impl/MiningMachineryServiceImpl.java
@@ -0,0 +1,41 @@
+package by.eftech.webapp.service.impl;
+
+
+import by.eftech.webapp.model.MiningMachinery;
+import by.eftech.webapp.repository.MiningMachineryRepository;
+import by.eftech.webapp.service.MiningMachineryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class MiningMachineryServiceImpl implements MiningMachineryService {
+
+    @Autowired
+    private MiningMachineryRepository repository;
+
+    @Override
+    public MiningMachinery save(MiningMachinery machineLocation) {
+        return repository.save(machineLocation);
+    }
+
+    @Override
+    public void delete(int id) {
+        repository.delete(id);
+    }
+
+    @Override
+    public MiningMachinery get(int id) {
+        return repository.get(id);
+    }
+
+    @Override
+    public void update(MiningMachinery truckMining) {
+        repository.save(truckMining);
+    }
+
+    @Override
+    public List<MiningMachinery> getAll() {
+        return repository.getAll();
+    }
+}
